4eaf7d0cf69d986576750681b573d44f1d81d0d9
rachel.wu <119239275@qq.com>
2025-12-29 14:23:46 +0800

    adding test cases

--- src/unit5/PriorityQueueClient.java
+++ src/unit5/PriorityQueueClient.java
@@ -15,10 +15,51 @@ public class PriorityQueueClient {
             System.out.println(pq.delMin());
         }
 
+    }
+
+    public static void testCase2(){
+        MinPQ<String> pq = new MinPQ<>(10);
+        pq.insert("Z");
+        pq.insert("A");
+        pq.insert("M");
+        pq.insert("B");
+
+        System.out.println("\nAfter insertion the queue is");
+        while (!pq.isEmpty()){
+            System.out.println(pq.delMin());
+        }
+    }
+
+    public static void testCase3(){
+        MinPQ<Integer> pq = new MinPQ<>(10);
+        pq.insert(1);
+        pq.insert(2);
+        pq.insert(3);
+        pq.insert(4);
+
+        System.out.println("\nAfter insertion the queue is");
+        while (!pq.isEmpty()){
+            System.out.println(pq.delMin());
+        }
+    }
+
+    public static void testResize(){
+        MinPQ<Integer> pq = new MinPQ<>(10);
+        for(int i = 20; i >=1; i--)
+            pq.insert(i);
+
+        System.out.println("\nAfter insertion the queue is");
+        while (!pq.isEmpty()){
+            System.out.println(pq.delMin());
+        }
+
 
     }
     public static void main(String[] args) {
         testCase1();
+        testCase2();
+        testCase3();
+        testResize();
 
     }
 }
